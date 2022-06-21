415bbb55519d63aa872f57420a6a476811566d50
Icaro Vieira <icarovsilva1@gmail.com>
2022-06-21 19:24:38 -0300

    feat: refactor

--- JS/scroll-menu.js
+++ JS/scroll-menu.js
@@ -1,16 +1,30 @@
-
 // Animação do Menu no Scroll da Página//
 
-$(function() {
-    $(window).on("scroll", function(){
-        if ($(window).scrollTop() > 768) {
-            $(".container").addClass("container_scroll");
-            $(".logo").addClass("logo_scroll");
-        } else {
-            $(".container").removeClass("container_scroll")
-            $(".logo").removeClass("logo_scroll");
-        }
-    })
-})
+// $(function () {
+//   $(window).on('scroll', function () {
+//     if ($(window).scrollTop() > 768) {
+//       $('.container').addClass('container_scroll');
+//       $('.logo').addClass('logo_scroll');
+//     } else {
+//       $('.container').removeClass('container_scroll');
+//       $('.logo').removeClass('logo_scroll');
+//     }
+//   });
+// });
+
+const $currentWindow = $(window);
+const $container = $('.container');
+const $logo = $('.logo');
+
+$($currentWindow).on('scroll', () => {
+  if ($currentWindow.scrollTop() > 768) {
+    $container.addClass('container_scroll');
+    $logo.addClass('logo_scroll');
+    return;
+  }
+
+  $container.removeClass('container_scroll');
+  $logo.removeClass('logo_scroll');
+});
 
 // Animação do Menu no Scroll da Página//
