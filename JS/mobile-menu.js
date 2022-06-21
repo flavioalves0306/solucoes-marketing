04354d50e6cef362f42c4fc6cd7787683bdebfb8
Flávio Alves <flavioalves0306@gmail.com>
2022-06-21 10:06:21 -0300

    add close menu

--- JS/mobile-menu.js
+++ JS/mobile-menu.js
@@ -1,14 +1,17 @@
-// Abrir Menu //
+// Abrir/Fechar Menu //
 
-function menuShow () {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = './img/bars-solid.svg';
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "./img/xmark-solid.svg";
-    }
+let menuMobile = document.querySelector(".mobile-menu");
+
+function menuShow() {
+  if (menuMobile.classList.contains("open")) {
+    menuClose();
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "./img/xmark-solid.svg";
+  }
 }
 
-// Abrir Menu //
+function menuClose() {
+  menuMobile.classList.remove("open");
+  document.querySelector(".icon").src = "./img/bars-solid.svg";
+}
