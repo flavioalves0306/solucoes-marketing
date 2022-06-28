a659b0db3ae6bde85162b92d1cb6a8e4c7403787
Flávio Alves <flavioalves0306@gmail.com>
2022-06-28 20:13:41 -0300

    add hero

--- JS/mobile-menu.js
+++ JS/mobile-menu.js
@@ -1,22 +1,19 @@
 // Abrir/Fechar Menu //
 
-const $menuMobile = $('.mobile-menu');
-const $icon = $('.icon');
+let menuMobile = document.querySelector(".mobile-menu");
 
 function menuShow() {
-  console.log($menuMobile.attr('class'));
-  if ($menuMobile.attr('class') === 'open') {
+  if (menuMobile.classList.contains('open')) {
     menuClose();
-    return;
+  } else {
+    menuMobile.classList.add('open');
+    document.querySelector(".icon").src = "./img/xmark-solid.svg";
   }
-  console.log($icon.attr('src'));
-  $menuMobile.addClass('open');
-  $icon.attr('src', './img/xmark-solid.svg');
 }
 
 function menuClose() {
-  $menuMobile.removeClass('open');
-  $icon.attr('src', './img/bars-solid.svg');
+  menuMobile.classList.remove('open');
+  document.querySelector(".icon").src = "./img/bars-solid.svg";
 }
 
 // Abrir/Fechar Menu //
--- JS/scroll-menu.js
+++ JS/scroll-menu.js
@@ -1,6 +1,5 @@
 // Animação do Menu no Scroll da Página//
 
-<<<<<<< HEAD
 $(function() {
     $(window).on("scroll", function(){
         if ($(window).scrollTop() > 600) {
@@ -12,33 +11,5 @@ $(function() {
         }
     })
 })
-=======
-// $(function () {
-//   $(window).on('scroll', function () {
-//     if ($(window).scrollTop() > 768) {
-//       $('.container').addClass('container_scroll');
-//       $('.logo').addClass('logo_scroll');
-//     } else {
-//       $('.container').removeClass('container_scroll');
-//       $('.logo').removeClass('logo_scroll');
-//     }
-//   });
-// });
-
-const $currentWindow = $(window);
-const $container = $('.container');
-const $logo = $('.logo');
-
-$($currentWindow).on('scroll', () => {
-  if ($currentWindow.scrollTop() > 768) {
-    $container.addClass('container_scroll');
-    $logo.addClass('logo_scroll');
-    return;
-  }
-
-  $container.removeClass('container_scroll');
-  $logo.removeClass('logo_scroll');
-});
->>>>>>> 8f6751fd8b1c89ec2002f479398ffbf5f98a290c
 
 // Animação do Menu no Scroll da Página//
